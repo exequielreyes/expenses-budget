66806b12f7d54377bb7767a2179d43550454984d
Leo Moreno <leonel_moreno99@hotmail.com>
2024-10-29 13:40:40 -0300

    add shadcn ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,36 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        'custom-black': "#27272A",
-        'custom-gray': "#717171",
-        'custom-green': "#089981",
-        'custom-red': "#F23645",
-        'custom-yellow': "#F6A000",
-        'custom-light-gray': "#ADADAD",
-        'custom-dark-gray': "#434343",
-      },
-      gridTemplateColumns: {
-        '19': 'repeat(19, minmax(0, 1fr))',
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			'custom-black': '#27272A',
+  			'custom-gray': '#717171',
+  			'custom-green': '#089981',
+  			'custom-red': '#F23645',
+  			'custom-yellow': '#F6A000',
+  			'custom-light-gray': '#ADADAD',
+  			'custom-dark-gray': '#434343'
+  		},
+  		gridTemplateColumns: {
+  			'19': 'repeat(19, minmax(0, 1fr))'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
   safelist: [
     'text-custom-green',
     'text-custom-yellow',
