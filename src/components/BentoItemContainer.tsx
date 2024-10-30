229e2ed47bee8520ee97bfc0895e3d7f245038df
Leo Moreno <leonel_moreno99@hotmail.com>
2024-10-30 17:50:00 -0300

    probando estilos xd

--- src/components/BentoItemContainer.tsx
+++ src/components/BentoItemContainer.tsx
@@ -8,12 +8,12 @@ type Props = {
 export const BentoItemContainer = ({ children, className }: Props) => {
 
   return (
-    <div className={`${className} rounded-2xl bg-custom-black h-fit`}>
+    <div className={`${className} rounded-2xl bg-custom-black/35 h-fit`}>
       <span className="relative inline-flex overflow-hidden rounded-2xl p-[1px] w-full h-full">
         <span
           className="absolute inset-0 w-full h-full bg-[linear-gradient(-10deg,rgba(255,255,255,0.1)_0%,rgba(255,255,255,0)_25%,rgba(255,255,255,0)_75%,rgba(255,255,255,0.1)_100%)]"
         ></span>
-        <div className={`w-full rounded-2xl bg-custom-black backdrop-blur-3xl ${className?.includes("table") ? "" : "p-4"}`}>
+        <div className={`w-full rounded-2xl bg-custom-black/35 backdrop-blur-3xl ${className?.includes("table") ? "" : "p-4"}`}>
           {children}
         </div>
       </span>
