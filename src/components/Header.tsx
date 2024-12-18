624738725a70b544e5935b79bca370b711324089
Leo Moreno <leonel_moreno99@hotmail.com>
2024-12-18 18:18:36 -0300

    change header desing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { UserMenu } from "@components/UserMenu";
+import { BentoItemContainer } from "./BentoItemContainer";
 
 export const Header = () => {
   return (
-    <header className="flex flex-col gap-4 bg-black h-[100px] w-full">
+    <BentoItemContainer className="m-auto max-w-[1670px] mt-4">
       <div className="flex justify-between max-w-[1670px] text-start m-auto w-full">
         <h1 className=" text-3xl font-bold ">Expenses Budget</h1>
         <UserMenu />
       </div>
-    </header>
+    </BentoItemContainer>
   );
 };
